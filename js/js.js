b741bc37079930d6d6fdfed2143129d93a85a156
Yue Ma <Yue Ma>
2017-01-17 16:56:03 +0800

    Signed-off-by: Yue Ma <Yue Ma>

--- js/js.js
+++ js/js.js
@@ -14,3 +14,16 @@ function search(){
 	var b = a.value;
 	window.location.href='https://www.baidu.com/s?ie=UTF-8&wd='+b;
 }
+
+function autoChangebg(){
+	var num = Math.random()*11;
+	// if (num>12) {
+	// 	num=num-10;
+	// }
+	//document.getElementsById('xx').background="img/lovewallpaper/5.jpg";
+	num=Math.round(num);
+	var b="img/"+num+".jpg";
+	// alert(name);
+	document.getElementById("body").background=b;
+
+}
